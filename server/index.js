e84656a16b0245c98c3322ac0273454fed597b2e
Arjun Goel <98057120+ARJ014@users.noreply.github.com>
2023-10-01 04:02:44 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(
-  cors({ origin: ["http://localhost:3000","https://frontfuntube.netlify.app"], credentials: true })
+  cors({ origin: "https://frontfuntube.netlify.app", credentials: true })
 );
 mongoose
   .connect(
